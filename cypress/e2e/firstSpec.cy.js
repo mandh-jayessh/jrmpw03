0a9aa294c1af29f881a814e9d9e680ecc3bdbebe
JRM <jrmandhyannee24@gmail.com>
2025-06-29 16:29:31 +0530

    Revert "cypress installation"
    This reverts commit f370dc4320a16ce41ad40e2fc65da7fbe95e813e.

--- cypress/e2e/firstSpec.cy.js
+++
@@ -1,9 +0,0 @@
-describe("Fill Details",()=>{
-
-    it("Fill Form",()=>{
-        cy.visit("https://demo.automationtesting.in/Register.html");
-        cy.get('[placeholder="First Name"]').type("Robert")
-        cy.get('[placeholder="Last Name"]').type("Senior")
-
-    })
-})
