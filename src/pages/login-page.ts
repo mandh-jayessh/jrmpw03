56c96fee8dfbbe42806d6557aca2e95bdf43ab36
JRM <jrmandhyannee24@gmail.com>
2025-06-29 18:03:45 +0530

    data parameterization

--- src/pages/login-page.ts
+++ src/pages/login-page.ts
@@ -5,12 +5,29 @@ export class LoginPage {
   username: Locator;
   password: Locator;
   submit: Locator;
+  error: Locator;
+  private creds: CredentialSet;
 
   constructor(page: Page) {
     this.page = page;
     this.username = page.locator("#username");
     this.password = page.locator("#password");
     this.submit = page.getByRole("button", { name: "submit" });
+    this.error = page.locator("div#error");
+    this.creds = {
+      validUser: {
+        username: "student",
+        password: "Password123",
+      },
+      incorrectUser: {
+        username: "incorrectUser",
+        password: "Password123",
+      },
+      incorrectPassword: {
+        username: "student",
+        password: "incorrectPassword",
+      },
+    };
   }
 
   async goto() {
@@ -22,12 +39,29 @@ export class LoginPage {
     );
   }
 
-  async fillCredentials() {
-    await this.username.fill("student");
-    await this.password.fill("Password123");
+  async fillCredentials(userType: string) {
+    const credentials = this.creds[userType];
+    if (!credentials) {
+      throw new Error(`Credentials not found for userType: "${userType}"`);
+    }
+    await this.username.fill(credentials.username);
+    await this.password.fill(credentials.password);
   }
 
   async submitCredentials() {
     await this.submit.click();
   }
+
+  async verifyErrorMessage(errorMessage: string) {
+    await expect(this.error).toHaveText(errorMessage);
+  }
+}
+
+export interface Creds {
+  username: string;
+  password: string;
+}
+
+export interface CredentialSet {
+  [key: string]: Creds;
 }
--- src/steps/login.steps.ts
+++ src/steps/login.steps.ts
@@ -4,8 +4,8 @@ Given(/^I am on the Login Page$/, async function () {
   await this.logIn.goto();
 });
 
-When(/^I provide the credentials and Click Submit$/, async function () {
-  await this.logIn.fillCredentials();
+When(/^I provide the (.*) credentials and Click Submit$/, async function (userType: string) {
+  await this.logIn.fillCredentials(userType);
   await this.logIn.submitCredentials();
 });
 
@@ -13,4 +13,8 @@ Then(/^I am logged in$/, async function () {
   await this.loggedIn.assertLoggedInPage();
   await this.loggedIn.assertLogInSuccess();
   await this.loggedIn.logOut();
+});
+
+Then(/^I validate the error message is (.*)$/, async function (errorMessage: string) {
+  await this.logIn.verifyErrorMessage(errorMessage)
 });
